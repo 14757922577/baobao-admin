6b4f91c24d4ef265bb3119ee6a85b6e8629e21f2
baba <123456@qq.com>
2020-12-30 14:01:58 +0800

    用户登录-创建页面组件并配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h3>后台管理系统发布</h3>
     <!-- 路由出口 -->
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 在VueCLI 创建的项目中 @ 表示 src 目录 
+// 他是 src 目录的路径别名
+// 好处：他不是当前文件目录影响
+// 注意：@就是 src 路径，后面别忘了写那个斜杠
+// 建议：如果加载的资源路径就在当前目录下，那就正常写
+//       如果需要进父级径查找的都使用 @
+import Login from '@/views/login/index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
